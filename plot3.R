2dd19e0ec2696de68a79eeb1b220f65586fc50de
Neil Fernandes <thirdfernandes@gmail.com>
2016-12-07 12:01:17 -0800

    Add plot 3.R

---
+++ plot3.R
@@ -0,0 +1,44 @@
+#Setting Working Directory
+
+setwd("~/Desktop/CourSera/Plotting")
+
+t <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+## Formatting date to Type Date
+t$Date <- as.Date(t$Date, "%d/%m/%Y")
+  
+## Filtering data set for wanted dates
+t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+  
+## Removing incomplete observations
+t <- t[complete.cases(t),]
+
+## Combining Date and Time columns
+dateTime <- paste(t$Date, t$Time)
+  
+## Naming the vector
+dateTime <- setNames(dateTime, "DateTime")
+  
+## Removing Date and Time column
+t <- t[ ,!(names(t) %in% c("Date","Time"))]
+  
+## Adding DateTime column
+t <- cbind(dateTime, t)
+  
+## Formating dateTime Column
+t$dateTime <- as.POSIXct(dateTime)
+
+## Creating Plot 3
+with(t, {plot(Sub_metering_1~dateTime, type="l",
+         ylab="Global Active Power (kilowatts)", xlab="")
+    lines(Sub_metering_2~dateTime,col='Red')
+    lines(Sub_metering_3~dateTime,col='Blue')
+  })
+  
+  legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
+         c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+         
+## Saving to file
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
+
